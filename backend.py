9721a4a469b0d4e041f06e68c0bfc68c29ef4a56
anurag-b-cumu <anurag.b@cumulations.com>
2025-01-31 11:20:28 +0000

    init

--- backend.py
+++ backend.py
@@ -21,9 +21,9 @@ class ProductReturnPredictor:
         self.X = self.df.drop(columns=['Returned'])
 
         scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(X)
+        X_scaled = scaler.fit_transform(self.X)
 
-        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42, stratify=y)
+        X_train, X_test, y_train, y_test = train_test_split(X_scaled, self.y, test_size=0.2, random_state=42, stratify=self.y)
 
         self.model = RandomForestClassifier(n_estimators=100, random_state=42)
         self.model.fit(X_train, y_train)
