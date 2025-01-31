0e7d37fe1f661ef8e9cf02c8cc89acd0b2b41f55
anurag-b-cumu <anurag.b@cumulations.com>
2025-01-31 11:22:46 +0000

    init

--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
 
 class ProductReturnPredictor:
     def __init__(self, file_path):
