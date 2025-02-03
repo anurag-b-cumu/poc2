5d668c219400f5efd7a86972632a2929318d39ed
anurag-b-cumu <anurag.b@cumulations.com>
2025-02-03 23:08:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ uploaded_file = "retail_sales_data.csv"
 
 predictor = ProductReturnPredictor(uploaded_file)
 
-# Get product list for selection
-product_list = predictor.list_prod()
-
-# Display training progress
 st.subheader("🎯 Training Random Forest Model...")
 rf_accuracy = predictor.train_model()
 st.success(f"✅ Model trained with {rf_accuracy}% accuracy!")
 
-# Select a product to predict return probability
-st.subheader("🔎 Select a product to predict return probability")
-selected_product_rf = st.selectbox("🛍️ Choose a product:", product_list, key="rf_product")
+Product, Customer = st.tabs(["Product", "Customer"])
+with Product:
+    product_list = predictor.list_prod()
+    
+    st.subheader("🔎 Select a product to predict return probability")
+    selected_product_rf = st.selectbox("🛍️ Choose a product:", product_list, key="rf_product")
+    
+    # Display prediction result
+    if selected_product_rf:
+        return_prob = predictor.get_return_probability_product(selected_product_rf)
+        st.header("📊 Prediction Result")
+        st.subheader(f'🔄 Return Probability: {return_prob}')
 
-# Display prediction result
-if selected_product_rf:
-    return_prob = predictor.get_return_probability(selected_product_rf)
-    st.header("📊 Prediction Result")
-    st.subheader(f'🔄 Return Probability: {return_prob}')
+with Customer:
+    product_list = predictor.list_customer()
+    
+    st.subheader("🔎 Select a customer to predict return probability")
+    selected_product_rf = st.selectbox("🛍️ Select Customer:", product_list, key="rf_product")
+    
+    # Display prediction result
+    if selected_product_rf:
+        return_prob = predictor.get_return_probability_customer(selected_product_rf)
+        st.header("📊 Prediction Result")
+        st.subheader(f'🔄 Return Probability: {return_prob}')
